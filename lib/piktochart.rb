523df53c6a4ce093244680619c8af9c8ef9127ca
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2025-05-08 16:56:12 +0200

    initial gem create

---
+++ lib/piktochart.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative "piktochart/version"
+
+module Piktochart
+end
---
+++ test/test_piktochart.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestPiktochart < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil Piktochart::VERSION
+  end
+
+  def test_sample_basket
+  end
+end
